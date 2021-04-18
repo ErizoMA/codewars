874172886e5f4d672f3cad5aae47a64b50784532
Erick Marcelo Aurazo <emarceloa@uni.pe>
2021-04-18 11:46:40 -0500

    Better solution

--- 6kyu-Unique-in-order.py
+++ 6kyu-Unique-in-order.py
@@ -1,24 +1,9 @@
 def unique_in_order(iterable):
-    # # Let secure that it is a string
-    # iterableStr = ""
-    # for i in iterable:
-    #     iterableStr += str(i)
-    # newList = []
-    # for i in range(len(iterableStr)-1):
-    #     if iterableStr[i] != iterableStr[i+1]:
-    #         newList.append(iterableStr[i])
-    # return newList + [iterableStr[-1]]
-    d = []
-    c = [x for x in iterable]
-    if len(iterable) > 0:
-        for i in range(len(c)-1):
-            if c[i] != c[i+1]:
-                d.append(c[i])
-        new = d.copy()
-        new.append(iterable[-1])
-        return new
-    else:
-        return []
+    newList = []
+    for i in iterable:
+        if len(newList) == 0 or i != newList[-1:]:
+            newList.append(i)
+    return newList
 
 
 print(unique_in_order('ABBCcAD'))
