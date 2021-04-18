02a1713895b9f88b9104b88dfeb1ba12e8bbd195
Erick Marcelo Aurazo <emarceloa@uni.pe>
2021-04-18 15:42:25 -0500

    Adding comments

--- 6kyu-Romans-number-decoder.py
+++ 6kyu-Romans-number-decoder.py
@@ -1,8 +1,9 @@
 roman = "MCMXC"
-
+# Dictionary
 romanDict = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
-
+# Sum
 number = 0
+# Defining which are + or -
 for i in range(len(roman)-1):
     if romanDict[roman[i]] >= romanDict[roman[i+1]]:
         number += romanDict[roman[i]]
